bcb9f44f3bebe712f3406f9f59ed544c2b8257c4
Miguel Johnson <miguelbjohnson@gmail.com>
2020-04-28 18:40:08 -0400

    test 'FizzBuzz' (div by 3 & 5) - pass

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,12 @@
 from helpers import fizzbuzz
-my_list = [3,5]
+my_list = [3,5,9,15,45]
 # def test_fizz():
+#    fizzbuzz(my_list)
+
+# def test_buzz():
 #     fizzbuzz(my_list)
-
-def test_buzz():
-    fizzbuzz(my_list)
     
+def test_fizzbuzz():
+    fizzbuzz(my_list)
 
-test_buzz()
+test_fizzbuzz()
--- helpers.py
+++ helpers.py
@@ -1,8 +1,12 @@
 def fizzbuzz(list_):
     for number in list_:
-        if number % 3 == 0:
+        if number % 3 == 0 and number % 5 == 0:
+            print(f"FizzBuzz")
+        elif number % 3 == 0:
             print(f"Fizz")
-        if number % 5 == 0:
+        elif number % 5 == 0:
             print(f"Buzz")
+        else:
+            pass
     
     
