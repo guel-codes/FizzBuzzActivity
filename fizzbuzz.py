f0da44bfde65db1027bc925bc16a98a326249fdc
Miguel Johnson <miguelbjohnson@gmail.com>
2020-04-28 18:26:33 -0400

    Test 'Fizz'(divisible by 3) requirement -pass

---
+++ fizzbuzz.py
@@ -0,0 +1,6 @@
+from helpers import fizzbuzz
+my_list = [3]
+def test_fizz():
+    fizzbuzz(my_list)
+
+test_fizz()
---
+++ helpers.py
@@ -0,0 +1,5 @@
+def fizzbuzz(list_):
+    for number in list_:
+        if number % 3 == 0:
+            print(f"Fizz")
+    
